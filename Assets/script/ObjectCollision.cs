a35e49f9a8dc8e2ffd02e83cae928eb92b1fd233
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-06-10 19:23:17 +0900

    爆発音
    爆発音

--- Assets/script/ObjectCollision.cs
+++ Assets/script/ObjectCollision.cs
@@ -2,9 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
 public class ObjectCollision : MonoBehaviour
 {   
     public GameObject explosionEffect; // エフェクトのプレハブをアサインする
+    public AudioClip collisionSound;    // 衝突時に再生する効果音
+    public float soundVolume = 1.0f;    // 効果音の音量
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = soundVolume;
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,6 +28,9 @@ public class ObjectCollision : MonoBehaviour
 
         // エフェクトを生成して再生する
         Instantiate(explosionEffect, collision.transform.position, Quaternion.identity);
+
+        // 効果音を再生する
+        audioSource.PlayOneShot(collisionSound);
     }
 }
 
