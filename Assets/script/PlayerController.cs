bb8b63b409f3055a95e152e861da1331e7530d50
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-06-10 22:38:31 +0900

    カメラ回転
    カメラ回転入れた

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    public float PlayerSpeed;
+    public float RotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotation();
+    }
+
+    void Move()
+    {
+        var speed = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            speed.z = PlayerSpeed;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            speed.z = -PlayerSpeed;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            speed.x = -PlayerSpeed;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            speed.x = PlayerSpeed;
+        }
+
+        transform.Translate(speed);
+    }
+
+    // 1個参照
+    void Rotation()
+    {
+        var speed = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed.y = -RotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed.y = RotationSpeed;
+        }
+        transform.eulerAngles += speed;
+    }
+}
